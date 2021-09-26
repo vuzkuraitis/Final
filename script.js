3e5e7a5abfec794dd8dc889c19d417d4e3708ba9
v.uzkuraitis@yahoo.com <Bybydejau1985>
2021-09-26 21:03:00 +0300

    swiper ir kelios modifikacijos

--- script.js
+++ script.js
@@ -21,3 +21,89 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0";
 }
+
+// myFunction validacija
+
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const phonenr = document.getElementById('phonenr');
+
+function myFunction(){
+  if(firstname.value == "")
+  {
+  alert("Įveskite vardą.");
+}
+else if (lastname.value == "")
+{
+    alert("Įveskite pavardę.");
+}
+else if(phonenr.value == "")
+{
+  alert("Įveskite savo telefono numerį.");
+}
+else{
+  document.getElementById('monthly').checked = true;
+  firstname.value = "";
+  lastname.value = "";
+  phonenr.value = "";
+  alert("Sukurta.")
+}
+}
+// myFunction2 validacija
+
+const firstname1 = document.getElementById('firstname1');
+const lastname1 = document.getElementById('lastname1');
+const phonenr1 = document.getElementById('phonenr1');
+
+function myFunction1(){
+  if(firstname1.value == "")
+  {
+  alert("Įveskite vardą.");
+}
+else if (lastname1.value == "")
+{
+    alert("Įveskite pavardę.");
+}
+else if(phonenr1.value == "")
+{
+  alert("Įveskite savo telefono numerį.");
+}
+else{
+  document.getElementById('personal').checked = true;
+  firstname1.value = "";
+  lastname1.value = "";
+  phonenr1.value = "";
+  alert("Sukurta.")
+}
+}
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+  }
+});
